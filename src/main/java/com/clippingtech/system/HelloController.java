2693a2934c8a092924fdfe647512a72b6ecec6aa
shantudas <shantuchandradas@yahoo.com>
2024-05-13 15:09:09 +0600

    db connection implemented

---
+++ src/main/java/com/clippingtech/system/HelloController.java
@@ -0,0 +1,13 @@
+package com.clippingtech.system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
--- src/main/java/com/clippingtech/system/SystemApplication.java
+++ src/main/java/com/clippingtech/system/SystemApplication.java
@@ -2,6 +2,8 @@ package com.clippingtech.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SystemApplication {
